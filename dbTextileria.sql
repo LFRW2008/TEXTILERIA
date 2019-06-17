1c446cfd3d7fdbe268f5a375fde8fb5675894d93
LFRW2008 <48104748+LFRW2008@users.noreply.github.com>
2019-06-17 18:22:08 -0500

    Update dbTextileria.sql

--- dbTextileria.sql
+++ dbTextileria.sql
@@ -6,6 +6,7 @@ go
 create table usuarios(
 id uniqueidentifier not null primary key,
 codigo char(15),
+acceso int,
 username varchar(100)not null,
 password varchar(max)not null,
 datecreate datetime
